3d41fe518209e192acefa451316dcbc4bbda1967
= <jackson.m.player@gmail.com>
2021-05-15 22:16:40 -0700

    Update to londondrugs

--- scraper.py
+++ scraper.py
@@ -5,8 +5,9 @@ from requests_html import HTMLSession
 
 def main():
     URLS = urls.parse_urls.get_urls('./urls/data/')
+    print(URLS)
     session = HTMLSession()
-    response = session.get(URLS['XBOX_SERIES_X_URLS']['londondrugs'])
+    response = session.get(URLS['XBOX_SERIES_S_URLS']['londondrugs'])
     response.html.render(timeout=10, sleep=7)
 
     info = parsers.londondrugs.parse(response)
