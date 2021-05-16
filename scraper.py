09946e5943150c30e944c330972c5d2d17ecb7a2
= <jackson.m.player@gmail.com>
2021-05-15 22:08:39 -0700

    Update scraper

--- scraper.py
+++ scraper.py
@@ -3,21 +3,13 @@ import urls
 
 from requests_html import HTMLSession
 
-# TODO: Implement this function
-def get_page_content(session, url):
-    response = session.get(url)
-    return response
-
-# TODO: Implement this function
-def get_info(content, parser):
-    pass
-
 def main():
     URLS = urls.parse_urls.get_urls('./urls/data/')
-    
     session = HTMLSession()
-    response = session.get(URLS['XBOX_SERIES_S_URLS'][1]['url'])
-    available = response.html.find('html', first=True).text
-    print(available)
+    response = session.get(URLS['XBOX_SERIES_X_URLS']['londondrugs'])
+    response.html.render(timeout=10, sleep=7)
+
+    info = parsers.londondrugs.parse(response)
+    print(info)
 if __name__ == "__main__":
    main()
