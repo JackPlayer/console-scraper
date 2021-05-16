98244dc2375f4906da8edf70e374586c0b9f0dcf
Jack <jackson.m.player@gmail.com>
2021-05-16 11:24:09 -0700

    Remove londondrugs

--- scraper.py
+++ scraper.py
@@ -5,12 +5,11 @@ from requests_html import HTMLSession
 
 def main():
     URLS = urls.parse_urls.get_urls('./urls/data/')
-    print(URLS)
     session = HTMLSession()
-    response = session.get(URLS['XBOX_SERIES_S_URLS']['londondrugs'])
+    response = session.get(URLS['XBOX_SERIES_X_URLS']['canadacomputers'])
     response.html.render(timeout=10, sleep=7)
 
-    info = parsers.londondrugs.parse(response)
+    info = parsers.canadacomputers.parse(response)
     print(info)
 if __name__ == "__main__":
    main()
