67fe1a3cf2411d5c4ba695770423f935c77f152f
= <jackson.m.player@gmail.com>
2021-05-16 20:46:56 -0700

    Add comments for parse_urls

--- urls/parse_urls.py
+++ urls/parse_urls.py
@@ -1,10 +1,25 @@
+"""
+Parses files containing urls to product pages
+"""
+
 def parse_file(file_path):
+    """Parse file with lines: retailer url
+
+    Args: 
+        file_path: The path to the url file
+
+    Returns:
+        dict: {'retailer1': 'url1', 'retailer2': 'url2'...}
+
+    """
     file = open(file_path)
     retailer_dict = {}
 
     for line in file:
         try: 
             words_in_line = len(line.split())
+
+            # Malformed line. 
             if (words_in_line != 2 and words_in_line != 0):
                 raise Exception(f'[Malformed URL File]: File - {file_path} | Line - {line}')
             [retailer, url] = line.split()
@@ -16,6 +31,14 @@ def parse_file(file_path):
     return retailer_dict
 
 def get_urls(base_dir):
+    """Gets the URLS from the console url files
+
+    Args:
+        base_dir: Directory where the url files are located
+    
+    Returns:
+        dict: {'console':{'retailer1':'url1',...},...}
+    """
     XBOX_SERIES_X_URLS = parse_file(f'{base_dir}xbox_series_x_urls.txt')
     XBOX_SERIES_S_URLS = parse_file(f'{base_dir}xbox_series_s_urls.txt')
     PS5_DISK_URLS = parse_file(f'{base_dir}ps5_disk_urls.txt')
